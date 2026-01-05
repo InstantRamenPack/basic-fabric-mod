d2d59d623959510efe545b2fb9e6126cff2a6243
InstantRamenPack <ray2009zhu@gmail.com>
2026-01-05 13:33:17 -0800

    Initial commit

---
+++ src/client/java/dev/modroll/basic/BasicClient.java
@@ -0,0 +1,10 @@
+package dev.modroll.basic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BasicClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
